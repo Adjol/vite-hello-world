4e60543c5968132e5357a4dd8ae70f41053fe2fb
Adjol <ad.mitrushi@gmail.com>
2023-12-20 17:20:09 +0100

    AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+    name: "AppTitle"
+}
+
+</script>
+
+<template>
+    <h1 class="main_tile">La mia prima App con Vite</h1>
+</template>
+
+<style scoped>
+.main_tile {
+    color: red;
+}
+</style>
